25ab2620ece1f91974100130f5a36097b772d3c5
MrLarkyy_ <studiotadikl@gmail.com>
2025-08-26 00:43:56 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Aquatic-gg/docs/',
+            'https://github.com/Aquatic-gg/',
         },
         blog: {
           showReadingTime: true,
